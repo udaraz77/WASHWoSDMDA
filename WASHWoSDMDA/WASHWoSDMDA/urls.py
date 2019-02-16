24eb032f7de98ab2564093ceab9edb216a8b4954
udaraz77 <umar.daraz@gmail.com>
2019-02-16 15:06:46 +0200

    Add project files.

---
+++ WASHWoSDMDA/WASHWoSDMDA/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for WASHWoSDMDA.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', WASHWoSDMDA.views.home, name='home'),
+    # url(r'^WASHWoSDMDA/', include('WASHWoSDMDA.WASHWoSDMDA.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
